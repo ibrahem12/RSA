93f6528b82cc369442f48347b866c5b9d1b04f60
ibrahem12 <ibrahiem.mohamed436@ci.menofia.edu.eg>
2019-02-25 22:22:07 +0200

    first commit

---
+++ src/security/Cipher.java
@@ -0,0 +1,20 @@
+package security;
+
+import java.math.BigInteger;
+
+
+/*
+ * 6. Transpoziční šifra: napište program, který pomocí transpozice zašifruje 
+ * nebo rozšifruje vstupní text ze souboru. Rozměry a přeházení sloupců zadává uživatel heslem.
+ * 
+ * Parametry: vstupní a výstupní soubory, heslo.
+ */
+/**
+ *
+ * @author sulcmil1
+ */
+public class Cipher {
+    public static void main(String[] args) {
+   
+    }      
+}
